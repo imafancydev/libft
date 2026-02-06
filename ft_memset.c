84f41764200f9aba73816a343fb93b837b3789e7
imafancydev <lucasaugustoleon@gmail.com>
2026-02-05 22:06:06 -0300

    refactor: correct loop counter type, remove test main function, and simplify loop syntax in `ft_memset`.

--- ft_memset.c
+++ ft_memset.c
@@ -2,35 +2,14 @@
 #include <stdio.h>
 
 void    *ft_memset(void *s, int c, size_t n){
-    int i;
-    char *str;
+    size_t  i;
+    char    *str;
 
     i = 0;
     str = (char *)s;
      
-    while(i < n){
+    while(i < n)
         str[i++] = c;
-    }
 
-    return s;
-
-}
-
-void printArray(int arr[], int n)
-{
-   for (int i=0; i<n; i++)
-      printf("%d ", arr[i]);
-}
-
-int main()
-{
-    int n = 10;
-    int arr[n];
-
-    // Fill whole array with 0.
-    ft_memset(arr, 0, n*sizeof(arr[0]));
-    printf("Array after memset()\n");
-    printArray(arr, n);
-
-    return 0;
+    return (void * )s;
 }
